feac12b64583eb62ac3f53ccd6894844b76f61f6
jasielserra <jasiel_serra@hotmail.com>
2022-07-04 22:03:48 -0300

    Incrementando barra de ferramentas.

--- main.py
+++ main.py
@@ -10,8 +10,16 @@ class Paintk:
         self.bar_menu = Frame(self.window, bg='#3b3b3b', height=50)
         self.bar_menu.pack(fill="x")
 
+        self.text_color = Label(self.bar_menu, text="  Color: ", fg="white", bg="#3b3b3b")
+        self.text_color.pack(side="left")
+
+        self.button_color = Button(self.bar_menu, bg="black", width=3, height=2, command=None).pack(side="left")
+
+
         self.area_draw = Canvas(self.window, height=720)
         self.area_draw.pack(fill="both")
+
+
 
         self.window.mainloop()
 
